67dd437d3f8c9187a3b0a8ce04a734235dc17e41
Colin <33527481+ArticWhite@users.noreply.github.com>
2022-04-29 20:41:20 -0400

    GridBasic
    basic grid system created

---
+++ Testing.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour{
+//creates the grid
+    public GridSystem grid;
+
+    public void Start(){ 
+        grid = new GridSystem(4 ,2 , 10f, new Vector3(20,0));
+        new GridSystem(2 ,2 , 10f, new Vector3(0,0));
+        new GridSystem(4 ,2 , 5f, new Vector3(-50,0));
+    }
+
+    public void Update(){
+        //grid.SetValue(1,1,56);
+        if (Input.GetMouseButtonDown(0)){
+            Debug.Log("here");
+            grid.SetValue(GJLib.GetMouseWorldPosition(), 56);
+        }
+
+        if (Input.GetMouseButtonDown(1)){
+            Debug.Log(grid.GetValue(GJLib.GetMouseWorldPosition()));
+        }
+    }
+
+    
+}
